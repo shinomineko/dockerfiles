52716a86808009a880d51eaf93232b78ff0d0ca5
Suracha Chowkuntod <sc@shinomin.me>
2024-09-09 01:12:05 +0700

    shfmt

--- build-all.sh
+++ build-all.sh
@@ -9,72 +9,72 @@ ERRORS="$(pwd)/errors"
 GIT_COMMIT_HASH="$(git rev-parse HEAD)"
 
 build_and_push() {
-	base=$1
-	suite=$2
-	build_dir=$3
+    base=$1
+    suite=$2
+    build_dir=$3
 
-	echo "building ${REPO}/${base}:${suite} for context ${build_dir}"
-	docker build --rm \
-		--label "org.opencontainers.image.title=${base}" \
-		--label "org.opencontainers.image.url=https://github.com/shinomineko/dockerfiles" \
-		--label "org.opencontainers.image.source=https://github.com/shinomineko/dockerfiles" \
-		--label "org.opencontainers.image.created=$(date --iso-8601=seconds)" \
-		--label "org.opencontainers.image.revision=${GIT_COMMIT_HASH}" \
-		-t "${REPO}/${base}:${suite}" "${build_dir}" || return 1
+    echo "building ${REPO}/${base}:${suite} for context ${build_dir}"
+    docker build --rm \
+        --label "org.opencontainers.image.title=${base}" \
+        --label "org.opencontainers.image.url=https://github.com/shinomineko/dockerfiles" \
+        --label "org.opencontainers.image.source=https://github.com/shinomineko/dockerfiles" \
+        --label "org.opencontainers.image.created=$(date --iso-8601=seconds)" \
+        --label "org.opencontainers.image.revision=${GIT_COMMIT_HASH}" \
+        -t "${REPO}/${base}:${suite}" "${build_dir}" || return 1
 
-	echo "===================================================================="
-	echo "successfully built ${REPO}/${base}:${suite} for context ${build_dir}"
-	echo "===================================================================="
+    echo "===================================================================="
+    echo "successfully built ${REPO}/${base}:${suite} for context ${build_dir}"
+    echo "===================================================================="
 
-	if [[ "$PUSH" == true ]]; then
-		docker push "${REPO}/${base}:${suite}"
-	fi
+    if [[ "$PUSH" == true ]]; then
+        docker push "${REPO}/${base}:${suite}"
+    fi
 }
 
 do_file() {
-	f=$1
-	image=${f%Dockerfile}
-	base=${image%%\/*}
-	build_dir=$(dirname "$f")
-	suite=${build_dir##*\/}
+    f=$1
+    image=${f%Dockerfile}
+    base=${image%%\/*}
+    build_dir=$(dirname "$f")
+    suite=${build_dir##*\/}
 
-	if [[ -z "$suite" ]] || [[ "$suite" == "$base" ]]; then
-		suite=latest
-	fi
+    if [[ -z "$suite" ]] || [[ "$suite" == "$base" ]]; then
+        suite=latest
+    fi
 
-	{
-		build_and_push "${base}" "${suite}" "${build_dir}"
-	} || {
-		echo "${base}:${suite}" >> "$ERRORS"
-	}
+    {
+        build_and_push "${base}" "${suite}" "${build_dir}"
+    } || {
+        echo "${base}:${suite}" >>"$ERRORS"
+    }
 }
 
 main() {
-	IFS=$'\n'
-	mapfile -t files < <(find -L . -iname '*Dockerfile' | sed 's|./||' | sort)
-	unset IFS
+    IFS=$'\n'
+    mapfile -t files < <(find -L . -iname '*Dockerfile' | sed 's|./||' | sort)
+    unset IFS
 
-	echo "running in parallel with ${JOBS} jobs"
-	parallel --verbose --tag --ungroup -j"${JOBS}" "$SCRIPT" do_file "{1}" ::: "${files[@]}"
+    echo "running in parallel with ${JOBS} jobs"
+    parallel --verbose --tag --ungroup -j"${JOBS}" "$SCRIPT" do_file "{1}" ::: "${files[@]}"
 
-	if [[ ! -f "$ERRORS" ]]; then
-		echo "[OK] no errors"
-	else
-		echo "[ERROR] some images did not build correctly, see below." >&2
-		echo "[ERROR] these images failed: $(cat "$ERRORS")" >&2
-		exit 1
-	fi
+    if [[ ! -f "$ERRORS" ]]; then
+        echo "[OK] no errors"
+    else
+        echo "[ERROR] some images did not build correctly, see below." >&2
+        echo "[ERROR] these images failed: $(cat "$ERRORS")" >&2
+        exit 1
+    fi
 }
 
 run() {
-	args=$*
-	f=$1
+    args=$*
+    f=$1
 
-	if [[ "$f" == "" ]]; then
-		main "$args"
-	else
-		$args
-	fi
+    if [[ "$f" == "" ]]; then
+        main "$args"
+    else
+        $args
+    fi
 }
 
 run "$@"
--- test.sh
+++ test.sh
@@ -10,38 +10,38 @@ VALIDATE_UPSTREAM="$(git rev-parse --verify FETCH_HEAD)"
 VALIDATE_COMMIT_DIFF="${VALIDATE_UPSTREAM}...${VALIDATE_HEAD}"
 
 validate_diff() {
-	if [[ "$VALIDATE_UPSTREAM" != "$VALIDATE_HEAD" ]]; then
-		git diff "$VALIDATE_COMMIT_DIFF" "$@"
-	else
-		git diff HEAD~ "$@"
-	fi
+    if [[ "$VALIDATE_UPSTREAM" != "$VALIDATE_HEAD" ]]; then
+        git diff "$VALIDATE_COMMIT_DIFF" "$@"
+    else
+        git diff HEAD~ "$@"
+    fi
 }
 
 IFS=$'\n'
 # shellcheck disable=SC2207
-changed_files=( $(validate_diff --name-only -- '*Dockerfile') )
+changed_files=($(validate_diff --name-only -- '*Dockerfile'))
 unset IFS
 
 # shellcheck disable=SC2068
 for f in ${changed_files[@]}; do
-	if ! [[ -e "$f" ]]; then
-		continue
-	fi
+    if ! [[ -e "$f" ]]; then
+        continue
+    fi
 
-	build_dir=$(dirname "$f")
-	base="${build_dir%%\/*}"
-	suite=${build_dir##*\/}
+    build_dir=$(dirname "$f")
+    base="${build_dir%%\/*}"
+    suite=${build_dir##*\/}
 
-	if [[ -z "$suite" ]] || [[ "$suite" == "$base" ]]; then
-		suite=latest
-	fi
+    if [[ -z "$suite" ]] || [[ "$suite" == "$base" ]]; then
+        suite=latest
+    fi
 
-	(
-	set -x
-	docker build -t "${base}:${suite}" "${build_dir}"
-	)
+    (
+        set -x
+        docker build -t "${base}:${suite}" "${build_dir}"
+    )
 
-	echo "============================================================"
-	echo "successfully built ${base}:${suite} for context ${build_dir}"
-	echo "============================================================"
+    echo "============================================================"
+    echo "successfully built ${base}:${suite} for context ${build_dir}"
+    echo "============================================================"
 done
